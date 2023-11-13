ace97077d87e477a9e6a0445b660fa74dcce3534
aleksandrovno <al.marquezm@duocuc.cl>
2023-11-13 16:58:01 -0300

    function recoveryPassword

--- src/app/olvide/olvide.page.ts
+++ src/app/olvide/olvide.page.ts
@@ -1,79 +1,45 @@
-import { Component, OnInit, ElementRef, ViewChildren } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import type { QueryList } from '@angular/core';
-import { Animation, AnimationController, IonCard } from '@ionic/angular'; 
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular'; // Importa NavController
+import { DataService } from '../data.service';
 
 @Component({
-  selector: 'app-recuperar-cuenta-code',
-  templateUrl: './recuperar-cuenta-code.page.html',
-  styleUrls: ['./recuperar-cuenta-code.page.scss'],
+  selector: 'app-olvide',
+  templateUrl: './olvide.page.html',
+  styleUrls: ['./olvide.page.scss'],
 })
-export class RecuperarCuentaCodePage implements OnInit {
-  codigoCambiaClave = 'CambiaClave';
+export class OlvidePage {
+  username: string = '';
+  recoveredPassword: string | null = null;
+  recoveredEmail: string | null = null;
+  userNotFound: boolean = false;
+  searchingCredentials: boolean = false;
 
-  formularioRecuperarCuentaCode: FormGroup;
+  constructor(private navCtrl: NavController, private dataService: DataService) {} // Inyecta NavController
 
-  @ViewChildren(IonCard, { read: ElementRef })
-  cardElements!: QueryList<ElementRef<HTMLIonCardElement>>;
+  recoverPassword() {
+    this.searchingCredentials = true;
 
-  private animation: Animation | undefined;
+    setTimeout(() => {
+      this.dataService.getUserByUsername(this.username).then((user) => {
+        if (user) {
+          this.recoveredPassword = user.password;
+          this.recoveredEmail = user.email;
+          this.userNotFound = false;
+        } else {
+          this.userNotFound = true;
+          // Reinicia las variables de contraseña y correo
+          this.recoveredPassword = null;
+          this.recoveredEmail = null;
+        }
 
-  constructor(
-    private animationCtrl: AnimationController,
-    private formBuilder: FormBuilder,
-    private router: Router
-  ) {
-    this.formularioRecuperarCuentaCode = this.formBuilder.group({
-      codigo: ['', Validators.required],
-    });
+        // Cambia el mensaje después de completar la búsqueda
+        this.searchingCredentials = false;
+      });
+    }, 5000); // 5000 ms = 5 segundos (simulación)
   }
 
-  ngOnInit() {}
-
-  ngAfterViewInit() {
-    if (this.cardElements && this.cardElements.length > 0) {
-      const firstCard = this.cardElements.first.nativeElement;
-
-      const card = this.animationCtrl
-        .create()
-        .addElement(firstCard)
-        .duration(2000)
-        .beforeStyles({
-          filter: 'invert(75%)',
-        })
-        .beforeClearStyles(['box-shadow'])
-        .afterStyles({
-          'box-shadow': 'rgba(255, 0, 50, 0.4) 0px 4px 16px 6px',
-        })
-        .afterClearStyles(['filter'])
-        .keyframes([
-          { offset: 0, transform: 'scale(1)' },
-          { offset: 0.5, transform: 'scale(1.5)' },
-          { offset: 1, transform: 'scale(1)' },
-        ]);
-
-      this.animation = this.animationCtrl.create().duration(2000).addAnimation([card]);
-    } else {
-      console.error('No se encontraron elementos en cardElements.');
-    }
-  }
-
-  cambiarClave() {
-    const codigoIngresado = this.formularioRecuperarCuentaCode.get('codigo')?.value;
-
-    if (codigoIngresado === this.codigoCambiaClave) {
-      console.log('Vamos a cambiar la clave');
-      
-      this.router.navigate(['/recuperar-cuenta-code2']);
-    } else {
-      if (this.animation) {
-        this.animation.play();
-      } else {
-        console.error('La propiedad "animation" es undefined.');
-      }
-      console.log('Código incorrecto');
-    }
-  
+  navigateLeft() {
+    // Navega de vuelta a la página anterior (por ejemplo, /home)
+    this.navCtrl.back();
   }
 }
